2ac484f36ddc0f18f38a32bd7cd7defaaeb7be1d
Ivan Alenin <aisproect@yandex.ru>
2021-09-26 22:47:13 +0300

    Test open weather api

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 import telegram
+import requests
+import json
+
+ow_token = token=os.environ["OPEN_WEATHER_SECRET_KEY"]
 
 def telegram_bot(request):
     bot = telegram.Bot(token=os.environ["BOT_SECRET_KEY"])
@@ -8,4 +12,16 @@ def telegram_bot(request):
         chat_id = update.message.chat.id
         # Reply with the same message
         bot.sendMessage(chat_id=chat_id, text=update.message.text)
+        weather = req_weather()
+        bot.sendMessage(chat_id=chat_id, json.dumps(weather))
     return "okay"
+
+def req_weather():
+    weather = ow_get_weather_by_location()
+    return weather["list"][0]["main"]
+
+def ow_get_weather_by_location(lat=51.509865, lon=-0.118092):
+    res = requests.get(f"https://api.openweathermap.org/data/2.5/find?lat={lat}&lon={lon}&cnt=1&appid={ow_token}")
+    if (res.status_code == 200):
+        return res.json()
+    pass
