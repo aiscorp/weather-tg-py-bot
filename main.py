41e724582770147caa818d41fd482dca53fd9f0b
Ivan Alenin <aisproect@yandex.ru>
2021-10-01 00:27:26 +0300

    firestore: bot main update for test

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import os
 import telegram
 import requests
-import json
 
-ow_token = token=os.environ["OPEN_WEATHER_SECRET_KEY"]
+from dbinstance import DBInstance
+
+db = DBInstance()
+
+ow_token = token = os.environ["OPEN_WEATHER_SECRET_KEY"]
+
 
 def telegram_bot(request):
     bot = telegram.Bot(token=os.environ["BOT_SECRET_KEY"])
+
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
+        message = update.message
+
         chat_id = update.message.chat.id
-        # Reply with the same message
-        bot.sendMessage(chat_id=chat_id, text=update.message.text)
+        user = update.message.chat.username
+
         weather = req_weather()
-        bot.sendMessage(chat_id=chat_id, text=json.dumps(weather))
-    return "okay"
+        bot.sendMessage(chat_id=chat_id,
+                        text=f"{user}, в Лондоне сейчас {weather.temp - 273.15}℃, а вложность {weather.humidity}%")
+
+        db.logs_add(message.to_dict())
+    return "OK"
+
 
 def req_weather():
     weather = ow_get_weather_by_location()
     return weather["list"][0]["main"]
+
 
 def ow_get_weather_by_location(lat=51.509865, lon=-0.118092):
     res = requests.get(f"https://api.openweathermap.org/data/2.5/find?lat={lat}&lon={lon}&cnt=1&appid={ow_token}")
